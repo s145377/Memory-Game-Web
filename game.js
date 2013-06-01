623587053fe1d02e55e5b6bb382a2e02d1aae88d
s145377 <s145377@students.lmsd.org>
2013-05-31 20:36:14 -0400

    removed unneeded files, cleaned up game.js

--- game.js
+++ game.js
@@ -1,15 +1,34 @@
 // JavaScript Document
-var tiles = [new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1),new tile(1)]
-function clickedTile(button) {
-	tiles[button.id] = new tile($(button).text())
-	tiles[button.id].number(5)
-	alert(tiles[button.id].number())
-}
-function tile(number) { //TODO: add color
-	function number() {
-		return number
+var tiles = new Array();
+var correct = new Array();
+var colors = 3;
+function setup() {
+	tiles = new Array();
+	for(var i = 0; i < 16; i++) {
+		alert(document.getElementById("0").text());
+		tiles.push(document.getElementById(i));
 	}
-	function number(n) {
-		number = n
+	reset();
+}
+function reset() {
+        for(var b in tiles) {
+	        $(b).show();
+		$(b).text("1");
+		$(b).css("background-color","red");
+		fixTextColor(b);
+	}
+	
+}
+function clickedTile(button) {
+	
+}
+function fixTextColor(button) {
+        var backcolor = $(button).css("background-color");
+        if(backColor!="undefined") {
+		var parts = backcolor.split("(")[1].split(")")[0].split(",");
+		var r = 255-parts[0];
+	        var g = 255-parts[1];
+        	var b = 255-parts[2];
+		$(button).css("color","rgb("+r+","+g+","+b+")");
 	}
 }
