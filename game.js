92fd10db3ca9a9a0dd13cdb7bae15bae29793457
s145377 <s145377@students.lmsd.org>
2013-05-31 22:02:00 -0400

    made the buttons become visible after they get css'd

--- game.js
+++ game.js
@@ -2,20 +2,14 @@
 var tiles = new Array();
 var correct = new Array();
 var colors = 3;
-function setup() {
+function reset() {
 	tiles = new Array();
 	for(var i = 0; i < 16; i++) {
-		alert(document.getElementById("0").text());
 		tiles.push(document.getElementById(i));
-	}
-	reset();
-}
-function reset() {
-        for(var b in tiles) {
-	        $(b).show();
-		$(b).text("1");
-		$(b).css("background-color","red");
-		fixTextColor(b);
+		$(tiles[i]).text("1");
+		$(tiles[i]).css("background-color","red");
+		fixTextColor(tiles[i]);		
+		$(tiles[i]).css("visibility","visible");
 	}
 	
 }
@@ -24,11 +18,9 @@ function clickedTile(button) {
 }
 function fixTextColor(button) {
         var backcolor = $(button).css("background-color");
-        if(backColor!="undefined") {
-		var parts = backcolor.split("(")[1].split(")")[0].split(",");
-		var r = 255-parts[0];
-	        var g = 255-parts[1];
-        	var b = 255-parts[2];
-		$(button).css("color","rgb("+r+","+g+","+b+")");
-	}
+	var parts = backcolor.split("(")[1].split(")")[0].split(",");
+	var r = 255-parts[0];	
+        var g = 255-parts[1];
+       	var b = 255-parts[2];
+	$(button).css("color","rgb("+r+","+g+","+b+")");
 }
