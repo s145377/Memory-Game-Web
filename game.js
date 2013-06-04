e8b9206667267afaaaff6f32b6b6282a9615e561
s145377 <s145377@students.lmsd.org>
2013-06-04 08:07:36 -0400

    ADded in vcolor changing

--- game.js
+++ game.js
@@ -2,19 +2,43 @@
 var tiles = new Array();
 var correct = new Array();
 var colors = 3;
+var white = "rgb(255, 255, 255)";
+var red = "rgb(255, 0, 0)";
+var blue = "rgb(0, 255, 255)";
+var green = "rgb(0, 255, 0)";
+
+
 function reset() {
 	tiles = new Array();
 	for(var i = 0; i < 16; i++) {
 		tiles.push(document.getElementById(i));
 		$(tiles[i]).text("1");
-		$(tiles[i]).css("background-color","red");
+		$(tiles[i]).css("background-color",white);
 		fixTextColor(tiles[i]);		
 		$(tiles[i]).css("visibility","visible");
 	}
 	
 }
 function clickedTile(button) {
+	var color = 0; //0=white,1=red, 2=blue, 3=limegreen
 	
+	if(     $(button).css("background-color")===white)
+		color = 1;
+	else if($(button).css("background-color")===red)
+		color = 2;
+	else if($(button).css("background-color")===blue)
+		color = 3;
+	else if($(button).css("background-color")===green)
+		color = 0;
+
+	if(     color==0)
+		$(button).css("background-color",white);
+	else if(color==1)
+		$(button).css("background-color",red);
+	else if(color==2)
+		$(button).css("background-color",blue);
+	else if(color==3)
+		$(button).css("background-color",green);
 }
 function fixTextColor(button) {
         var backcolor = $(button).css("background-color");
@@ -55,4 +79,4 @@ var tilesCheck = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 		}
 		
 	}
-	*\
+*/
