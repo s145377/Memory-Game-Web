0dd0a79017eac2cc3b7bfde3d5f28fd1aa837ca1
s145377 <s145377@students.lmsd.org>
2013-06-05 15:43:49 -0400

    Added lives display

--- game.js
+++ game.js
@@ -5,6 +5,18 @@ var white = "rgb(255, 255, 255)";
 var red = "rgb(255, 0, 0)";
 var blue = "rgb(0, 255, 255)";
 var green = "rgb(0, 255, 0)";
+var lives = 3;
+var level;
+var colors;
+var time;
+var canChange = new Boolean();
+
+var LEVEL_TIME_DECREASE = .98;
+var START_TIME = 5000;
+var START_LEVEL = 1;
+var START_LIVES = 3;
+var START_COLORS = 2; // includes white
+var COLOR_ORDER = new Array();
 
 function reset() {
 	tiles = new Array();
@@ -18,19 +30,39 @@ function reset() {
 	
 }
 function generate() {
+	reset();
 	correct = new Array();
 	for(var i = 0; i < 16; i++) {
-		var color = Math.floor(Math.random() * 4)+1;
-		correct[i] = color;		
+		var color = Math.floor(Math.random() * 4);
+		correct[i] = color;	
 		changeColor(tiles[i], color);
 	}
 }
+function setLevel(level) {
+	this.level = level;
+	$(document.getElementById("level")).text("Level - "+level);
+}
 function clickedTile(button) {
-	var color = colorToNumber(button)+1; //0=white,1=red, 2=blue, 3=limegreen
-	if(color>3)
-		color = 0;
-	changeColor(button, color);
-	tiles[button.id] = color;
+	if(canChange) {
+		var color = colorToNumber(button)+1; //0=white,1=red, 2=blue, 3=limegreen
+		if(color>3)
+			color = 0;
+		changeColor(button, color);
+	}
+}
+function restart() {
+	setLevel(START_LEVEL);
+	setLives(START_LIVES);
+	colors = START_COLORS;
+	time = START_TIME;
+
+	doLevel();
+}
+function setLives(lives) {
+	this.lives = lives;
+	$(document.getElementById("lives")).text("Lives - "+lives);
+	if(lives<0)
+		restart();
 }
 function colorToNumber(button) {
 	if(     $(button).css("background-color")===white)
@@ -44,10 +76,26 @@ function colorToNumber(button) {
 	return -1;
 
 }
+function checkLevel() {
+	if(!check()) {
+		lives--;
+		restart();
+	}
+	else {
+		setLevel(level+1);
+       		time*=LEVEL_TIME_DECREASE;
+		doLevel();
+	}
+}
+function doLevel() {
+       canChange = false;
+       generate();
+       setTimeout(function() {reset(); canChange = true}, time);
+
+}
 function changeColor(button, color) {
-        if(     color==0)
-                $(button).css("background-color",white);
-        else if(color==1)
+        $(button).css("background-color",white);
+        if(color==1)
                 $(button).css("background-color",red);
         else if(color==2)
                 $(button).css("background-color",blue);
@@ -62,7 +110,7 @@ function fixTextColor(button) {
        	var b = 255-parts[2];
 	$(button).css("color","rgb("+r+","+g+","+b+")");
 }
-function levelCheck() {
+function check() {
 	for (var i = 0; i < 16; i++) {
 		if (correct[i] != colorToNumber(tiles[i])) {
 			return false;
@@ -71,34 +119,3 @@ function levelCheck() {
 	return true;
 }
 
-//Old Code
-
-/*
-var tiles = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-var tilesCheck = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-	function clickedTile(button) {
-	
-	if (tiles[button.id] == 1){
-		tiles[button.id] = 0
-	}
-	else{
-		tiles[button.id] += 1
-	}
-	var ID = button.id
-	alert(tiles[button.id])
-	}
-
-	function checkTile() {
-		
-	}
-	
-	function createPattern() {
-		for (var i=0;i<tilesCheck.length;i++){
-			var randomnumber=Math.floor(Math.random()*2)
-			tilesCheck[i] = randomnumber
-			tiles[i] = randomnumber
-		}
-		
-	}
-*/
