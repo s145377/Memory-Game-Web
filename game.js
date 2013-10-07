3f3da226dbbf0b0e98ce7a1d1e6600de7129b5ce
s145377 <s145377@students.lmsd.org>
2013-10-06 21:24:59 -0400

    debugging

--- game.js
+++ game.js
@@ -3,20 +3,20 @@
 /*
 
 levels	    	    	      - number of levels in this game,	    	    	    	    	    -1 for unlimited levels
-startLives	    	      - number of lives the user is given at start,
+startLives	    	        - number of lives the user is given at start,
 	    	    	    	    	    	    rounded down if not a whole number	    	    	      -1 for unlimited lives
-liveIncrease	    	    - percent number of lives is increased per
+liveIncrease	    	      - percent number of lives is increased per
 	    	    	    	    	    	    successful level completion
-startLevelTime	      - amount of time board is shown before first level,
+startLevelTime	          - amount of time board is shown before first level,
 	    	    	    	    	    	    in milliseconds	    	    	    	    	    	    	    	     -1 for unlimited time
-levelTimeDecrease   - percent decrease in levelTime per level,
+levelTimeDecrease         - percent decrease in levelTime per level,
 	    	    	    	    	    	    decreased at level start
 colors	    	    	      - array of tile colors
-startTileColors	     - number of tile colors on first level, rounded
+startTileColors	          - number of tile colors on first level, rounded
 	    	    	    	    	    	    down if not a whole number 
-tileColorIncrease   - percent increase in number of tile colors per
+tileColorIncrease         - percent increase in number of tile colors per
 	    	    	    	    	    	    level
-startLevel	    	      - level that the user starts on
+startLevel	    	        - level that the user starts on
 
 */
 var tiles = new Array();
@@ -92,7 +92,8 @@ var Game = function (startLives, levels, startLevelTime, levelTimeDecrease, colo
 	    function reset() {
 	    	    for(var i = 0; i < 16; i++) {
 	    	    	    tiles.push(document.getElementById(i));
-	    	    	    changeColor(tiles[i],colors[0]);	    	    
+	    	    	    changeColor(tiles[i],colors[0]);	   
+	    	    	 //  	$(tiles[i]).text(0));
 	    	    	    $(tiles[i]).css("visibility","visible");
 	    	    }
 	    }
@@ -113,17 +114,16 @@ var Game = function (startLives, levels, startLevelTime, levelTimeDecrease, colo
 
 
 function changeColor(button, color) {
-	    $(button).css("background-color",colors[color]);
-	    $(button).text(color);
-	    
+	    $(button).css("background-color",color);
+
 	    //fix text color
 	    var backColor = color;
 	    var parts = backColor.replace('(', '')
-	    	    	    	    	    	     .replace(')', '')
-	    	    	    	    	    	     .split(",");
+	    	    	    	 .replace(')', '')
+	    	    	    	 .split(",");
 	    var r = 255-parts[0];	    
-	    	    var g = 255-parts[1];
-	       var b = 255-parts[2];
+	    var g = 255-parts[1];
+	    var b = 255-parts[2];
 	    $(button).css("color","rgb("+r+","+g+","+b+")");
 }
 
